0348e7ad79b54ed8ea36cac1ca884caca04f238e
Steve Kaliski <59668283+stevekaliski-stripe@users.noreply.github.com>
2026-03-24 15:11:54 -0400

    Update Purl formula to version 0.2.1

--- Formula/purl.rb
+++ Formula/purl.rb
@@ -2,8 +2,8 @@
 class Purl < Formula
   desc 'A curl-esque CLI for making HTTP requests that require payment.'
   homepage 'https://purl.dev'
-  url 'https://github.com/stripe/purl/archive/refs/tags/v0.2.0.tar.gz'
-  sha256 '5af1eeca3a07da97ebd30c001349c9bb01c02e7affe5c5f15cad0d34745ad541'
+  url 'https://github.com/stripe/purl/archive/refs/tags/v0.2.1.tar.gz'
+  sha256 'b427ca8c0b18874d8aef0d6f44f940ea5a86e8f68764abea722bd5a0b0875cb6'
   license 'MIT'
 
   depends_on 'rust' => :build
